51de9910e8df514b9434a4b61ddd0114bc6c8a5b
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-05-06 23:08:51 +0530

    Ability To Check Fare For Premium Rides

--- src/main/java/com/invoice/CabOperations.java
+++ src/main/java/com/invoice/CabOperations.java
@@ -1,14 +1,25 @@
 package com.invoice;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Locale;
 
 public class CabOperations {
     private static final double FARE_PER_KM = 10;
     private static final double FARE_PER_MINUTE = 1;
-    private static final double MINIMUM_FARE = 5;
-    public Map<Integer, double[]> cabOperationsMap = new HashMap<>();
-    public static Integer userId;
+    private static final double MINIMUM_FARE = 5.0;
 
+    private static final double PREMIUM_FARE_PER_KM = 15;
+    private static final double PREMIUM_FARE_PER_MINUTE = 2;
+    private static final double PREMIUM_MINIMUM_FARE = 20;
+
+    public Map<Integer, double[]> cabOperationsMap = new HashMap<>();
+    public Integer userId;
+    public String typeOfTheCustomer;
+
+
+    public CabOperations(String  customerType){
+        this.typeOfTheCustomer = customerType;
+    }
     public CabOperations() {
     }
 
@@ -27,16 +38,55 @@ public class CabOperations {
         return totalRideCost;
     }
 
+
+    public CabOperations(Integer userId, String customerType) {
+        this.userId = userId;
+        this.typeOfTheCustomer = customerType;
+        cabOperationsMap.put(userId,null);
+    }
+
+
+
+    public double calculateTotalRideCost(double totalRidingDistance, double totalRidingTimeInHours) {
+
+        if (this.typeOfTheCustomer.toLowerCase(Locale.ROOT).startsWith("r")) {
+            double totalRidingTimeInMin = totalRidingTimeInHours * 60;
+            double totalFare = totalRidingDistance * FARE_PER_KM + totalRidingTimeInMin * FARE_PER_MINUTE;
+
+            return Math.max(totalFare, MINIMUM_FARE);
+        }
+        if (this.typeOfTheCustomer.toLowerCase(Locale.ROOT).startsWith("p")) {
+            double totalRidingTimeInMin = totalRidingTimeInHours * 60;
+            double totalFare = totalRidingDistance * PREMIUM_FARE_PER_KM + totalRidingTimeInMin * PREMIUM_FARE_PER_MINUTE;
+
+            return Math.max(totalFare, PREMIUM_MINIMUM_FARE);
+        }
+        else
+            return 0.0;
+    }
+
+    public double calculateTotalRideCost(CabRide[] multipleRides){
+        double totalRideCost = 0;
+        for (CabRide ride :multipleRides) {
+            totalRideCost += calculateTotalRideCost(ride.rideDistance,ride.timeInMin);
+        }
+        return totalRideCost;
+    }
+
     public double calculateAverageCostForRides(CabRide[] rides){
         double totalRideCost =  0;
         for(CabRide ride : rides){
-            totalRideCost += calculateCostOfRide(ride.rideDistance,ride.timeInMin);
+            totalRideCost += calculateTotalRideCost(ride.rideDistance,ride.timeInMin);
         }
         return totalRideCost / rides.length;
     }
 
     public Integer getUserId(){
         return userId;
+    }
+
+    public String getTypeOfTheCustomer(Integer userId){
+        return typeOfTheCustomer;
     }
 
     public double[] getRideDetails(Integer userId){
@@ -49,7 +99,7 @@ public class CabOperations {
 
     public void addUserRideDetails(CabOperations user, CabRide[] rides){
         double[] userTotalRideDetails = {user.calculateAverageCostForRides(rides),
-                user.calculateMultipleRideCost(rides),
+                user.calculateTotalRideCost(rides),
                 user.getNumberOfRides(rides)};
         cabOperationsMap.put(user.getUserId(),userTotalRideDetails);
     }
--- src/test/java/com/invoice/CabOperationsTest.java
+++ src/test/java/com/invoice/CabOperationsTest.java
@@ -55,6 +55,25 @@ public class CabOperationsTest {
 
         Assertions.assertArrayEquals(new double[]{7650.0, 22950.0, 3.0},firstUser);
         Assertions.assertArrayEquals(new double[]{8773.333333333334, 26320.0, 3.0},secondUser);
+    }
+    @Test
+    public void givenCustomerType_ShouldReturnRegardingCostWithRespectTo_Customer(){
+        CabOperations firstPremiumUserRideDetails = new CabOperations( 12345,"Premium");
+        CabOperations firstRegularUserRideDetails = new CabOperations(34343,"Regular");
+        CabRide[] totalRidesOfPremiumUser = {(new CabRide(25,2))
+                ,(new CabRide(50.0,1.5))
+                ,(new CabRide(60.0,2.5))};
+        CabRide[] totalRidesOfRegularUser = {(new CabRide(22,4))
+                ,(new CabRide(60.0,2.5))
+                ,(new CabRide(30.0,0.5))};
+        firstPremiumUserRideDetails.addUserRideDetails(firstPremiumUserRideDetails,totalRidesOfPremiumUser);
+        firstRegularUserRideDetails.addUserRideDetails(firstRegularUserRideDetails,totalRidesOfRegularUser);
+
+        double[] firstUser = firstPremiumUserRideDetails.getRideDetails(firstPremiumUserRideDetails.getUserId());
+        double[] secondUser = firstRegularUserRideDetails.getRideDetails(firstRegularUserRideDetails.getUserId());
+
+        Assertions.assertArrayEquals(new double[]{15075.0, 45225.0, 3.0},firstUser);
+        Assertions.assertArrayEquals(new double[]{8773.333333333334, 26320.0, 3.0},secondUser);
 
     }
 }
