1f228ec3c59ede20158de571bd5ac1cc292a7b46
ShouryaSharma17 <shouryasharma17@gmail.com>
2021-05-06 22:55:34 +0530

    Ability To Get List Of Rides When User ID is Provided

--- src/main/java/com/invoice/CabOperations.java
+++ src/main/java/com/invoice/CabOperations.java
@@ -1,10 +1,16 @@
 package com.invoice;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CabOperations {
     private static final double FARE_PER_KM = 10;
     private static final double FARE_PER_MINUTE = 1;
     private static final double MINIMUM_FARE = 5;
-    private Object CabRide;
+    public Map<Integer, double[]> cabOperationsMap = new HashMap<>();
+    public static Integer userId;
+
+    public CabOperations() {
+    }
 
     public double calculateCostOfRide(double totalRidingDistance, double totalRidingTimeInHours){
         double totalRidingTimeInMin = totalRidingTimeInHours * 60;
@@ -21,15 +27,30 @@ public class CabOperations {
         return totalRideCost;
     }
 
-    public int getNumberOfRides(CabRide[] numberOfRides){
-        return numberOfRides.length;
-    }
-
     public double calculateAverageCostForRides(CabRide[] rides){
         double totalRideCost =  0;
         for(CabRide ride : rides){
             totalRideCost += calculateCostOfRide(ride.rideDistance,ride.timeInMin);
         }
         return totalRideCost / rides.length;
+    }
+
+    public Integer getUserId(){
+        return userId;
+    }
+
+    public double[] getRideDetails(Integer userId){
+        return cabOperationsMap.get(userId);
+    }
+
+    public int getNumberOfRides(CabRide[] numberOfRides){
+        return numberOfRides.length;
+    }
+
+    public void addUserRideDetails(CabOperations user, CabRide[] rides){
+        double[] userTotalRideDetails = {user.calculateAverageCostForRides(rides),
+                user.calculateMultipleRideCost(rides),
+                user.getNumberOfRides(rides)};
+        cabOperationsMap.put(user.getUserId(),userTotalRideDetails);
     }
 }
--- src/test/java/com/invoice/CabOperationsTest.java
+++ src/test/java/com/invoice/CabOperationsTest.java
@@ -2,8 +2,6 @@ package com.invoice;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class CabOperationsTest {
     CabOperations cabOperations = new CabOperations();
 
@@ -36,5 +34,27 @@ public class CabOperationsTest {
         Assertions.assertEquals(30150,totalCabFareCost);
         Assertions.assertEquals(3,numberOfRides);
         Assertions.assertEquals(10050,averageCostOfMultipleRides);
+    }
+    @Test
+    public void givenUserId_shouldReturnParticularRidesAndTheirInvoice(){
+        CabOperations firstUserToRide = new CabOperations();
+        CabOperations secondUserToRide = new CabOperations();
+
+        CabRide[] totalRidesOfFirstUser = {(new CabRide(25,2))
+                ,(new CabRide(50.0,1.5))
+                ,(new CabRide(60.0,2.5))};
+        CabRide[] totalRidesOfSecondUser = {(new CabRide(22,4))
+                ,(new CabRide(60.0,2.5))
+                ,(new CabRide(30.0,0.5))};
+
+        firstUserToRide.addUserRideDetails(firstUserToRide,totalRidesOfFirstUser);
+        secondUserToRide.addUserRideDetails(secondUserToRide,totalRidesOfSecondUser);
+
+        double[] firstUser = firstUserToRide.getRideDetails(firstUserToRide.getUserId());
+        double[] secondUser = secondUserToRide.getRideDetails(secondUserToRide.getUserId());
+
+        Assertions.assertArrayEquals(new double[]{7650.0, 22950.0, 3.0},firstUser);
+        Assertions.assertArrayEquals(new double[]{8773.333333333334, 26320.0, 3.0},secondUser);
+
     }
 }
